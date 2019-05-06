02228b8e49fe2cc72788e93efc0ce7a85eb11366
Runnan Li <rli12@wou.edu>
2019-05-06 15:21:22 -0700

    Displayed but not working reCAPTCHA ERROR: Invalid domain for site key

---
+++ reCAPTCHA/reCAPTCHA/Scripts/reCAP.js
@@ -0,0 +1,9 @@
+﻿// Specify onload callback function.  This function will get called when all the dependencies have loaded.
+var onloadCallback = function () {
+    grecaptcha.render('divCap', {
+        'sitekey': '6LfZ5aAUAAAAAI6slx8IOItXPZGyq5Hupmy8uh29',
+        'callback': function (response) {
+            $('#reCAP').val('t');
+        }
+    })
+};
