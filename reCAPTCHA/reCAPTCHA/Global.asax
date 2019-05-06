ed8288633a1f53c3cbdb2020fd9e812086d59ec8
Runnan Li <rli12@wou.edu>
2019-05-06 15:32:27 -0700

    update dev

---
+++ reCAPTCHA/reCAPTCHA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="reCAPTCHA.MvcApplication" Language="C#" %>
---
+++ reCAPTCHA/reCAPTCHA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(reCAPTCHA.Startup))]
+namespace reCAPTCHA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
